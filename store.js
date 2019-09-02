b4294773ef133268da5b38aeb1296931b95751a2
Tochukwu <tnzewi@gmail.com>
2019-09-02 14:30:52 +0100

    saver on the redux persist

--- store.js
+++ store.js
@@ -1,10 +1,21 @@
 import { createStore, applyMiddleware} from 'redux';
 import {persistStore} from 'redux-persist';
 import ReduxThunk from "redux-thunk";
+import AsyncStorage from '@react-native-community/async-storage'
+
 
 import rootReducer from './Reducers/RootReducer';
 
-const middewares = [ReduxThunk];
+const saver = store => next => action => {
+    let result = next(action);
+    let stateToSave = store.getState();
+        AsyncStorage.setItem("lawyer_Tickets", JSON.stringify(stateToSave));
+        return result;
+
+}
+
+
+const middewares = [ReduxThunk, saver];
 
 
 export const store = createStore(rootReducer, applyMiddleware(...middewares));
